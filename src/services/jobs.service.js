f10f4af18af3b01d35304ee5da35aed1b5532ed7
TahaHamdy <104000401+TahaHamdy02@users.noreply.github.com>
2023-12-10 20:41:20 +0200

    Revert "servre"
    This reverts commit f9fb9739a57b212d470c35d96730e5a8ef4778ae.

--- src/services/jobs.service.js
+++
@@ -1,8 +0,0 @@
-const apiServices = require("./servicesHandler");
-const Job = require("../modules/jop.module");
-
-exports.getOneJob = apiServices.getOne(Job);
-exports.getAllJobs = apiServices.getAll(Job);
-exports.updateJob = apiServices.updateFunction(Job);
-exports.deleteJob = apiServices.deleteOne(Job);
-exports.createJob = apiServices.createOne(Job);
